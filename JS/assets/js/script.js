dbe437d3ac3c6c5d7219640860e8e4afc06814d0
Mashrur <mashrurrafiu.mash@gmail.com>
2024-03-20 11:49:48 +0600

    For Loop

--- JS/assets/js/script.js
+++ JS/assets/js/script.js
@@ -1,22 +1,10 @@
-let Marks=60;
-
-switch(true){
-
-case(Marks<=100 && Marks>=80):
-    document.write("A+");
-break;
-case(Marks<80 && Marks>=70):
-    document.write("A");
-break;
-case(Marks<70 && Marks>=60):
-    document.write("A-");
-break;
-case(Marks<60 && Marks>=50):
-    document.write("B")
-break;
-case(Marks<50 && Marks>=40):
-    document.write("C")
-break;
-default:
-    document.write("F")
+var i;
+for(i=10;i<100;i=i+10){
+    if(i==30){
+        continue;
+    }
+    if(i==70){
+        break;
+    }
+    document.write(i+"<button>Submit</button><br>");
 }
