2cc62771aab2bd3974c1b69344d524e43d87f859
Mashrur <mashrurrafiu.mash@gmail.com>
2024-03-20 12:16:30 +0600

    Do While Loop

--- JS/assets/js/script.js
+++ JS/assets/js/script.js
@@ -7,10 +7,17 @@ for(i=10;i<100;i=i+10){
         break;
     }
     document.write(i+"<button>For Loop</button><br>");
-}*/
+}
 
 var i=10;
 while(i<100){
     document.write(i+"<button>While Loop</button><br>");
     i=i+10;
 }
+*/
+var i=10;
+do{
+    document.write(i+"<button>Do While Loop</button><br>")
+    i=i+10
+}
+while(i<100)
