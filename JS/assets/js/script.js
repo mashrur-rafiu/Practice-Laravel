3c4bcf84fbaf5f0739fbb66ed00c805a5afbe458
Mashrur <mashrurrafiu.mash@gmail.com>
2024-03-20 10:48:20 +0600

    Marks IF Else

--- JS/assets/js/script.js
+++ JS/assets/js/script.js
@@ -1,4 +1,20 @@
-var x=['Dhaka', 'Rangpur', 'Rajshahi'];
+let Marks=14;
 
-document.write(x);
-console.log(x);
+if(Marks>=80 && Marks<=100){
+    document.write("A+");
+}
+else if(Marks>=70){
+    document.write("A");
+}
+else if(Marks>=60){
+    document.write("A-");
+}
+else if(Marks>=50){
+    document.write("B")
+}
+else if(Marks>=40){
+    document.write("C")
+}
+else{
+    document.write("F")
+}
