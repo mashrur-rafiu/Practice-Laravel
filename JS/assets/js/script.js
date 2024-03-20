f24396cdfef02db85f09124ccd456933fbe69e36
Mashrur <mashrurrafiu.mash@gmail.com>
2024-03-20 12:52:23 +0600

    Function String

--- JS/assets/js/script.js
+++ JS/assets/js/script.js
@@ -1,6 +1,6 @@
-function subFunction(n1, n2){
-    let sub=n1+n2
-    document.write(sub+"<br>")
+function myName(name){
+    document.write("My name is "+name+".<br>")
 }
-subFunction(50,95)
-subFunction(13,77)
+myName("Mashrur")
+myName("Borshon")
+myName("Bokor")
