42d2b243b798c559ddd16042ad3a4145da1273a7
Mashrur <mashrurrafiu.mash@gmail.com>
2024-03-21 14:36:26 +0600

    For In Loop Object

--- JS/assets/js/script.js
+++ JS/assets/js/script.js
@@ -1,4 +1,10 @@
-let students = ["Mashrur", "Deen", "Emon", "Faysal", "Tayeb"];
+let students = {
+    Name: "Mashrur Rafiu",
+    Age: "24",
+    Occupation: "Student",
+    Study: "Daffodil International University"
+
+}
 
 for(let item in students){
     document.write(students[item]+"<br>")
